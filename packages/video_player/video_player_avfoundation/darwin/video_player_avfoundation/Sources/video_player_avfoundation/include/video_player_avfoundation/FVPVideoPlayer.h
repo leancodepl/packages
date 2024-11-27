f373efc8bc02ed386dc6fe6489f6eaac2eb7b95f
Paweł Jakubowski <pawel.jakubowski@leancode.pl>
2024-11-27 13:59:39 +0100

    Add missing doc comments

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/include/video_player_avfoundation/FVPVideoPlayer.h
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/include/video_player_avfoundation/FVPVideoPlayer.h
@@ -46,21 +46,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/// Initializes a new instance of FVPVideoPlayer with the given URL, frame updater, display link,
-/// HTTP headers, AV factory, and registrar.
+/// Initializes a new instance of FVPVideoPlayer with the given URL, HTTP headers, AV factory, and
+/// registrar.
 - (instancetype)initWithURL:(NSURL *)url
                 httpHeaders:(nonnull NSDictionary<NSString *, NSString *> *)headers
                   avFactory:(id<FVPAVFactory>)avFactory
                   registrar:(NSObject<FlutterPluginRegistrar> *)registrar;
 
-/// Initializes a new instance of FVPVideoPlayer with the given AVPlayerItem, frame updater, display
-/// link, AV factory, and registrar.
+/// Initializes a new instance of FVPVideoPlayer with the given AVPlayerItem, AV factory, and
+/// registrar.
 - (instancetype)initWithPlayerItem:(AVPlayerItem *)item
                          avFactory:(id<FVPAVFactory>)avFactory
                          registrar:(NSObject<FlutterPluginRegistrar> *)registrar;
 
-/// Initializes a new instance of FVPVideoPlayer with the given asset, frame updater, display link,
-/// AV factory, and registrar.
+/// Initializes a new instance of FVPVideoPlayer with the given asset, AV factory, and registrar.
 - (instancetype)initWithAsset:(NSString *)asset
                     avFactory:(id<FVPAVFactory>)avFactory
                     registrar:(NSObject<FlutterPluginRegistrar> *)registrar;
