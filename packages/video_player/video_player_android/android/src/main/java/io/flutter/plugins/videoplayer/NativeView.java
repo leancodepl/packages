52fa6aa21c63d3d66cf4e9b22c1dd9f8bcac5a5f
Paweł Jakubowski <pawel.jakubowski@leancode.pl>
2024-12-16 13:27:19 +0100

    Revert "Use TextureView instead of SurfaceView"
    This reverts commit ea7e586faf405c70270054c881e774aac4848d09.

--- packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/NativeView.java
+++ packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/NativeView.java
@@ -5,64 +5,28 @@
 package io.flutter.plugins.videoplayer;
 
 import android.content.Context;
-import android.graphics.SurfaceTexture;
-import android.view.Surface;
-import android.view.TextureView;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
-import androidx.media3.common.VideoSize;
 import androidx.media3.exoplayer.ExoPlayer;
 import io.flutter.plugin.platform.PlatformView;
 
 class NativeView implements PlatformView {
-  @NonNull private final TextureView textureView;
-  private Surface videoSurface;
+  @NonNull private final SurfaceView surfaceView;
 
   NativeView(@NonNull Context context, int id, @NonNull ExoPlayer exoPlayer) {
-    textureView = new TextureView(context);
-    textureView.setSurfaceTextureListener(
-        new TextureView.SurfaceTextureListener() {
-          @Override
-          public void onSurfaceTextureAvailable(
-              @NonNull SurfaceTexture surface, int width, int height) {
-            videoSurface = new Surface(surface);
-            exoPlayer.setVideoSurface(videoSurface);
-          }
-
-          @Override
-          public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-            // No implementation needed.
-          }
-
-          @Override
-          public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-            exoPlayer.setVideoSurface(null);
-            if (videoSurface != null) {
-              videoSurface.release();
-              videoSurface = null;
-            }
-            return true;
-          }
-
-          @Override
-          public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-            // No implementation needed.
-          }
-        });
+    surfaceView = new SurfaceView(context);
+    exoPlayer.setVideoSurfaceView(surfaceView);
   }
 
   @NonNull
   @Override
   public View getView() {
-    return textureView;
+    return surfaceView;
   }
 
   @Override
   public void dispose() {
-    if (videoSurface != null) {
-      videoSurface.release();
-      videoSurface = null;
-    }
+    surfaceView.getHolder().getSurface().release();
   }
 }
